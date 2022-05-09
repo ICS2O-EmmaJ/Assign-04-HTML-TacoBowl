b978ccbd465cc0af5337d2613cfef2bc6778e994
Emma Janani <99275087+EmmaJanani13@users.noreply.github.com>
2022-05-09 18:15:46 +0000

    Button is fixed!!!

--- js/script.js
+++ js/script.js
@@ -6,37 +6,54 @@
 
  function displayCost() {
   // declaring variables
-  let baseCost = "Please make a selection above."
+  let baseCost = "Please make a selection above.";
    
 	// get user input
-  let size = document.getElementById('size');
+  let size = document.getElementById('size').value;
 
-  // determining the number of checkboxes checked (number of toppings selected)
-  let checkboxes =
-document.querySelectorAll('input[type="checkbox"]:checked');
-  let numToppings = checkboxes.length;
-
-  // declaring constant price
-  const TOPPING_PRICE = 0.50
-
-  toppingCost = numToppings * TOPPING_PRICE
-
-  // if statements
+  // calculating base cost using if statements
   if (size == "pequena") {
-	baseCost = "The cost is 7.45"
+  	baseCost = 7.45;
 	}
 	else if (size == "mediana") {
-	baseCost = "The cost is 9.35"
+  	baseCost = 9.35;
 	}
 	else if (size == "grande") {		
-	baseCost = "The cost is 11.95"
-	}
+  	baseCost = 11.95;
+	}    
+   
+  // determining how many toppings the user selected by counting number of checkboxes checked (code taken from https://www.techiedelight.com/count-number-check-boxes-javascript/)
+  let numCheckboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+  let numToppings = numCheckboxes.length;
 
-  const TOPPINGPRICE = 0.50
-  toppingNumber = 
-  topping cost = 
-   
+  // declaring constant topping price
+  const TOPPING_PRICE = 0.50;
+
+  // determing topping price
+  let toppingCost = numToppings * TOPPING_PRICE;
+
+  // declaring variables
+  let sideCost = 0;
+
+  // determining whether user wants a side order or not by determining which radio button is selected (code taken from https://www.geeksforgeeks.org/how-to-check-whether-a-radio-button-is-selected-with-javascript/#:~:text=Using%20Input%20Radio%20checked%20property,id%20is%20check%20or%20not.)
+  if(document.getElementById('yes').checked) {
+    sideCost = 4.00;
+  }
+    
+  if(document.getElementById('no').checked) {
+    sideCost = 0;
+  }
+
+  // calculating subtotal
+  let subtotal = baseCost + toppingCost + sideCost;
+
+  // calculating tax
+  const HST = 0.13;
+  let tax = subtotal * HST;
+
+  // calculating total
+  let total = subtotal + tax;
+  
   // display the greeting
-	document.getElementById('display-results').innerHTML = baseCost
+	document.getElementById('display-results').innerHTML = "Your subtotal is $" + subtotal.toFixed(2) +".<br>Your HST is $" + tax.toFixed(2) + ".<br>Your total is $" + total.toFixed(2) + ".";
 }
-   
